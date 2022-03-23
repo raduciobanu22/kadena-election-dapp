2d7e4b8a879cd1c8b903ee345dcf5071f9e94dba
Radu Ciobanu <radu.ciobanu22@gmail.com>
2022-03-23 14:11:12 +0800

    Basic UI and loading votes

--- src/App.js
+++ src/App.js
@@ -1,23 +1,55 @@
-import logo from './logo.svg';
+import { useState, useEffect } from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
+import Pact from 'pact-lang-api';
+import { Container, Row, Col, Button } from 'react-bootstrap';
+
+// const NETWORK_ID = 'testnet04';
+// const CHAIN_ID = '1';
+const API_HOST = `http://localhost:8080`;
+
+const getVotes = (candidate) => {
+  const cmd = {
+    pactCode: `(simple-vote.getVotes "${candidate}")`
+  };
+  return Pact.fetch.local(cmd, API_HOST);
+}
+
+const submitVote = async () => {
+
+}
 
 function App() {
+  const [votesA, setVotesA] = useState(0);
+  const [votesB, setVotesB] = useState(0);
+
+  useEffect(() => {
+    getVotes('A').then(response => {
+      setVotesA(response.result.data)
+    })
+
+    getVotes('B').then(response => {
+      setVotesB(response.result.data)
+    })
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App mt-5">
+      <Container>
+        <h2>Election Demo</h2>
+        <Row>
+          <Col className="text-center">
+            <h5>Candidate A</h5>
+            <p>Votes: {votesA}</p>
+            <Button className="mt-2">Vote</Button>
+          </Col>
+          <Col className="text-center">
+            <h5>Candidate B</h5>
+            <p>Votes: {votesB}</p>
+            <Button className="mt-2">Vote</Button>
+          </Col>
+        </Row>
+      </Container>
     </div>
   );
 }

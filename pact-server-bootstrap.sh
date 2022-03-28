8c3be384bbfca461c160d3d1f17b2c7133a4905f
Radu Ciobanu <radu.ciobanu22@gmail.com>
2022-03-28 19:51:23 +0800

    Added pact server bootstrap script

---
+++ pact-server-bootstrap.sh
@@ -0,0 +1,18 @@
+set -m
+
+# Start pact server in background
+pact -s config.yaml & 2>/dev/null
+
+# Get background job number
+jnum=$(jobs -l | grep " $! " | sed 's/\[\(.*\)\].*/\1/')
+
+sleep 2
+
+echo 'Loading fungible-v2...'
+CONTRACT_PATH=pact/coin/fungible-v2.pact node scripts/pact-server/deploy.js
+
+echo 'Loading coin-v3...'
+CONTRACT_PATH=pact/coin/coin-v3.pact node scripts/pact-server/deploy.js
+
+# Bring pact server to foreground
+fg $jnum > /dev/null

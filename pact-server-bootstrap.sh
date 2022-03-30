2ddde6b10d5d3aef019328a096eb921d1a4d13a0
Radu Ciobanu <radu.ciobanu22@gmail.com>
2022-03-30 11:48:22 +0800

    Moved coin and fungible to pact/root

--- pact-server-bootstrap.sh
+++ pact-server-bootstrap.sh
@@ -9,10 +9,10 @@ jnum=$(jobs -l | grep " $! " | sed 's/\[\(.*\)\].*/\1/')
 sleep 2
 
 echo 'Loading fungible-v2...'
-CONTRACT_PATH=pact/coin/fungible-v2.pact node scripts/pact-server/deploy.js
+CONTRACT_PATH=pact/root/fungible-v2.pact node scripts/pact-server/deploy.js
 
 echo 'Loading coin-v3...'
-CONTRACT_PATH=pact/coin/coin-v3.pact node scripts/pact-server/deploy.js
+CONTRACT_PATH=pact/root/coin-v3.pact node scripts/pact-server/deploy.js
 
 # Bring pact server to foreground
 fg $jnum > /dev/null
